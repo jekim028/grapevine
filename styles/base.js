83dca52d91046e021c48afb470f542bc3c984369
Jenna Kim <jekim028@gmail.com>
2023-11-30 14:58:46 -0800

    base styling

---
+++ styles/base.js
@@ -0,0 +1,31 @@
+export const padding = {
+  sm: 8,
+  med: 16,
+  lg: 24,
+};
+
+export const borderRadius = {
+  reg: 8,
+  pill: 100,
+};
+
+export const colors = {
+  grapevine: "#8C69C6",
+  textPrimary: "#000000",
+  textInverted: "#FFFFFF",
+  textSecondary: "#D9D9D9",
+  formBackground: "#ECECEE",
+  placeholder: "#8e8e8e",
+};
+
+export const fonts = {
+  micro: 8,
+  sm: 14,
+  reg: 17,
+  lg: 19,
+  t3: 24,
+  t2: 32,
+  t1: 44,
+};
+
+export const iconSize = 24;
